328e891092b75dfb3590cc1c6734550c4a7481d8
jnflint <jnflint@users.noreply.github.com>
2025-02-13 10:51:22 +1100

    Update tile_info.rs charge preferred image order

--- src/iiif/tile_info.rs
+++ src/iiif/tile_info.rs
@@ -50,7 +50,7 @@ static QUALITY_ORDER: [&str; 5] = ["bitonal", "gray", "color", "native", "defaul
 
 // Image formats, from least favorite to favorite
 // webp is the least favorite because of this bug: https://github.com/image-rs/image/issues/939
-static FORMAT_ORDER: [&str; 7] = ["webp", "gif", "bmp", "tif", "jpg", "jpeg", "png"];
+static FORMAT_ORDER: [&str; 7] = ["webp", "gif", "bmp", "tif","png", "jpg", "jpeg"];
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum TileSizeFormat {
